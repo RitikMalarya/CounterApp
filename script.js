35991f1e89fd19846e6de8f3f196468b7ef38723
Ritik Malarya <65644535+RitikMalarya@users.noreply.github.com>
2023-04-02 13:31:04 +0530

    added index and script working

---
+++ script.js
@@ -0,0 +1,14 @@
+const counterValue = document.querySelector("#counter");
+
+const Increment = () => {
+    let value = parseInt(counterValue.innerText);
+    value = value + 1;
+    counterValue.innerText = value;
+}
+const decrement = () => {
+    let value = parseInt(counterValue.innerText);
+    value = value - 1;
+    counterValue.innerText= value;
+}
+
+
